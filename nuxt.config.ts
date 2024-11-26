8c3da577f6280e147cb30b116e6879e95a87985d
Dmitri <dklassen@remtechindustries.com>
2024-11-26 11:35:00 -0500

    Add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,27 @@ export default defineNuxtConfig({
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Sora:wght@600&family=Space+Grotesk:wght@700&family=Space+Grotesk&display=swap'
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png'
+        },
+        {
+          rel: 'manifest',
+          href: '/site.webmanifest'
         }
       ],
       titleTemplate: '%s | RemTech Industries',
