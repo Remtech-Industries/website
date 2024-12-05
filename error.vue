e7cbaa2224eb3fe9f9f54143af035c76d50a8a67
Dmitri <dklassen@remtechindustries.com>
2024-12-05 13:36:27 -0500

    Create custom 404 page

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="lg:text-lg xl:text-xl 2xl:text-2xl">
+        <div class="relative h-svh bg-remtech-red text-gray-50">
+            <div class="text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl text-center absolute w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-screen-2xl px-4 md:px-8 lg:px-12 xl:px-16">
+                <h1 class="text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl block sora mb-2 lg:mb-4 xl:mb-6">
+                    404: Page Not Found
+                </h1>
+                <p class="mx-4 md:mx-5 lg:mx-6 xl:mx-8 2xl:mx-10">Return to the <NuxtLink to="/" class="underline">Home</NuxtLink> page.</p>
+            </div>
+        </div>
+    </div>
+</template>
