21a26454cd61de6bc70ef0f7545ee4574f5b0724
Dmitri <dklassen@remtechindustries.com>
2024-12-10 09:19:37 -0500

    Debug cookie popup

--- components/CookiePrompt.vue
+++ components/CookiePrompt.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed z-20 print:hidden bottom-0 left-0 w-full p-4 sm:p-6 lg:p-8 bg-red-700 text-gray-50" v-show="open">
+  <div class="fixed z-20 print:hidden bottom-0 left-0 w-full p-4 sm:p-6 lg:p-8 bg-red-700 text-gray-50" v-show="open && mounted">
     <p class="mb-4">Our website uses cookies to help us better serve our customers. For more information, please read our <NuxtLink to="/cookie-policy" class="underline">cookie policy</NuxtLink>.</p>
     <div class="flex justify-end flex-col sm:flex-row items-end">
       <button @click="acceptAll" class="underline mb-2 sm:mb-0 sm:ml-4">Accept all cookies</button>
@@ -14,9 +14,9 @@ const consent = useCookie('consent', {
     maxAge: 34560000
 })
 const open = ref(!consent.value)
-watch(consent, (n, o) => {
-  console.dir(n)
-  console.dir(o)
+const mounted = ref(false)
+onMounted(() => {
+  mounted.value = true
 })
 
 function acceptAll() {
